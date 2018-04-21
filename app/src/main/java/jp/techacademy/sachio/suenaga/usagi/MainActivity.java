3fb1c566bb5c4e92ed0a96d82be4ea26a877b42d
sachio0013 <sachio0013@gmail.com>
2018-04-21 20:49:37 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/sachio/suenaga/usagi/MainActivity.java
@@ -0,0 +1,40 @@
+package jp.techacademy.sachio.suenaga.usagi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //　卵が割れる数
+    int usagiCount = 30;
+
+    public void onUsagi (View v){
+
+        //　画像のリモコン
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+
+        //　カウントダウン
+        usagiCount--;
+
+        //　画像を変える+条件分岐
+        if( usagiCount<20)iv.setImageResource(R.drawable.usagi2);
+        if( usagiCount<15)iv.setImageResource(R.drawable.usagi3);
+        if( usagiCount<10)iv.setImageResource(R.drawable.usagi4);
+        if( usagiCount<5)iv.setImageResource(R.drawable.usagi5);
+
+        //　アニメーション処理
+        iv.startAnimation(AnimationUtils.loadAnimation(this, R.anim.tap));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.setImageResource(R.drawable.usagi1);
+    }
+}
